840825c8189f9e9c50b7985f56c4cb716bf292c1
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-08 22:59:12 -0600

    seeds file, fixed schema

--- db/migrate/20210409011033_create_enrollments.rb
+++ db/migrate/20210409011033_create_enrollments.rb
@@ -1,7 +1,7 @@
 class CreateEnrollments < ActiveRecord::Migration[6.1]
   def change
     create_table :enrollments do |t|
-      t.string :points
+      t.integer :total_points
       t.belongs_to :classroom, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
 
