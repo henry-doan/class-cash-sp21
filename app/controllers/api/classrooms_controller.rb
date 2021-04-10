f087a76875ef603ba241291589d213f64fa221fa
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-09 21:36:06 -0600

    classroom controller

--- app/controllers/api/classrooms_controller.rb
+++ app/controllers/api/classrooms_controller.rb
@@ -1,2 +1,39 @@
 class Api::ClassroomsController < ApplicationController
+
+  def index
+    render json: Classroom.all
+  end
+
+  def show
+    @classroom = Classroom.find(params[:id])
+    render json: @classroom
+  end
+
+  def create
+    @classroom = Classroom.new(classroom_params)
+    if @classroom.save
+      render json: @classroom
+    else
+      render json: { errors: @classroom.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @classroom = Classroom.find(params[:id])
+    if @classroom.update(classroom_params)
+      render json: @classroom
+    else
+      render json: { errors: @classroom.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Classroom.find(params[:id]).destroy
+    render json: { message: 'classroom deleted' }
+  end
+  
+  private
+    def classroom_params
+      params.require(:classroom).permit(:name)
+    end
 end
