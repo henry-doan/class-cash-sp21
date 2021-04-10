8c526e9cd85c9e833c2aebbc9f3ba409da9cbce2
Harry Dhillon <navbirsdhillon@gmail.com>
2021-04-09 21:15:04 -0600

    enrollment controller and initial homepage created

--- app/controllers/api/enrollments_controller.rb
+++ app/controllers/api/enrollments_controller.rb
@@ -1,2 +1,36 @@
 class Api::EnrollmentsController < ApplicationController
+
+  def index
+    render json: current_user.enrollments 
+  end
+
+  def create 
+    @enrollment = current_user.enrollments.new(enrollment_params)
+    if @enrollment.save
+      render json: @enrollment
+    else
+      render json: { errors: @enrollment.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update 
+    @enrollment = current_user.enrollments.find(params[:id])
+    if @enrollment.update(enrollment_params)
+      render json: @enrollment
+    else
+      render json: { errors: @enrollment.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @enrollment = current_user.enrollments.find(params[:id])
+    @enrollment.destroy
+    render json: { message: 'Enrollment Deleted!' }
+  end
+
+  private 
+    def enrollment_params
+      params.require(:enrollment).permit(:total_points)
+    end
+
 end
--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,7 +1,17 @@
+import { Container, Header, Divider, List, Button } from "semantic-ui-react";
 
 const Home = () => (
   <>
-    <h1>Test Home page</h1>
+    <Divider hidden />
+    <Container textAlign='center'>
+      <Header>Welcome to ClassCash</Header>
+      To get started, you need to be a part of a Classroom. You can join a Classroom in multiple ways.
+      <List>
+        <List.Item>1. Ask an owner of an existing Classroom to add you by your email.</List.Item>
+        <List.Item>2. Start your own Classroom.</List.Item>
+      </List>
+      <Button size='large' style={{color: '#ffffff', backgroundColor: '#1CB993' }} >Start a new Classroom</Button> 
+    </Container>
   </>
 )
 export default Home;
