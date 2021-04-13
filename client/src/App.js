f53308a7abb145e9b6c9b7744e5731a6dcd8dc38
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-13 00:13:36 -0600

    ClassroomList, delete

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,8 @@ import Earn from './components/points/Earn';
 import AdminRewards from './components/rewards/AdminRewards';
 import Spend from './components/rewards/Spend';
 import CreateClassroom from './components/classrooms/CreateClassroom';
+import ClassroomProvider from './providers/ClassroomProvider'
+
 const App = () => (
   <>
     <Navbar />
