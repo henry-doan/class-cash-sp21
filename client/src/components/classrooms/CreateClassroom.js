f4c95831d3b09209eea8f310e93ebb2d45ea424d
Sean Schmidt <schmidtsean048@gmail.com>
2021-04-14 10:31:17 -0600

    created link and flash

--- client/src/components/classrooms/CreateClassroom.js
+++ client/src/components/classrooms/CreateClassroom.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import { Form, Segment, Button, Header } from "semantic-ui-react";
+import { Form, Segment, Button, Header, Modal, Icon } from "semantic-ui-react";
 import { ClassroomConsumer } from '../../providers/ClassroomProvider';
+import {Link} from 'react-router-dom';
+
 const CreateClassroom = ({addClassroom}) => {
   const [classroom, setClassroom] = useState({ name: '' })
  
@@ -24,7 +26,18 @@ const CreateClassroom = ({addClassroom}) => {
         onChange={(e, { value }) => setClassroom({ ...classroom, name: value })}
         />
       <Segment textAlign='center' basic>
-        <Button style={{color: '#ffffff', backgroundColor: '#1CB993' }} type='submit'>Submit</Button>
+          <Modal 
+            trigger={<Button style={{color: '#ffffff', backgroundColor: '#1CB993' }} type='submit'>Submit</Button>}>
+              <Header textAlign='center'>Classroom Created. Continue To Classrooms</Header>
+                <Modal.Actions>
+                  <Button color='green' inverted>
+                    <Icon name="checkmark" /> Ok
+                  </Button>
+                </Modal.Actions>
+          </Modal>
+        <br />
+        <br />
+        <Button style={{backgroundColor: '#1CB993' }}><Link style={{color: 'white'}}to="/classroomselect">Go to Classrooms</Link></Button>
       </Segment>
       </Form>
   </Segment>
