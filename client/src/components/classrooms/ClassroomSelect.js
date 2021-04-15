3e31a576619eb1774a6f4c3b1d818c46089f1124
Sean Schmidt <schmidtsean048@gmail.com>
2021-04-15 17:14:02 -0600

    Dashboard

--- client/src/components/classrooms/ClassroomSelect.js
+++ client/src/components/classrooms/ClassroomSelect.js
@@ -1,24 +1,9 @@
 import ClassroomList from './ClassroomList'
-import React, { useState } from 'react'
 
-const ClassroomSelect = () => {
-  const [isSelectingClassroom, setIsSelectingClassroom] = useState(false)
 
-const handleClick = () => {
-  setIsSelectingClassroom(!isSelectingClassroom)
-}
-
-return (
+const ClassroomSelect = () => (
   <>
-    <button onClick={handleClick}>Swap State</button>
-    {isSelectingClassroom ? <ClassroomList /> : <MyClassroomHeader />}
-    <h1>my rewards</h1>
-    <h1>activities</h1>
+    <ClassroomList />
   </>
-  )
-}
-export default ClassroomSelect;
-
-const MyClassroomHeader = () => (
-  <h1>DASHBOARD HERE</h1>
 )
+export default ClassroomSelect;
