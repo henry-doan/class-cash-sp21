da8a9df4d7da2eb832aa42e431b8d627c33ede67
Sean Schmidt <schmidtsean048@gmail.com>
2021-04-14 23:47:17 -0600

    list to dashboard switch

--- client/src/components/classrooms/ClassroomSelect.js
+++ client/src/components/classrooms/ClassroomSelect.js
@@ -1,8 +1,24 @@
 import ClassroomList from './ClassroomList'
+import React, { useState } from 'react'
 
-const ClassroomSelect = () => (
+const ClassroomSelect = () => {
+  const [isSelectingClassroom, setIsSelectingClassroom] = useState(false)
+
+const handleClick = () => {
+  setIsSelectingClassroom(!isSelectingClassroom)
+}
+
+return (
   <>
-    <ClassroomList/>
+    <button onClick={handleClick}>Swap State</button>
+    {isSelectingClassroom ? <ClassroomList /> : <MyClassroomHeader />}
+    <h1>my rewards</h1>
+    <h1>activities</h1>
   </>
-)
+  )
+}
 export default ClassroomSelect;
+
+const MyClassroomHeader = () => (
+  <h1>DASHBOARD HERE</h1>
+)
