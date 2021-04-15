68ca225f96afef2ce0c108e18ee55f342643d5a9
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-15 00:10:13 -0600

    closer to solving MyPoints

--- client/src/components/enrollments/Enrollment.js
+++ client/src/components/enrollments/Enrollment.js
@@ -1,20 +1,50 @@
-import EnrollmentConsumer from '../../providers/EnrollmentProvider'
+import AuthConsumer from '../../providers/AuthProvider'
+import { useState, useEffect, Component } from 'react'
+import axios from 'axios'
+import { Segment } from 'semantic-ui-react'
 
-const Enrollment = ({classroomId, showEnrollment}) => {
+const Enrollment = ({classroomId, user}) => {
+  const [enrollment, setEnrollment] = useState([])
+
+  // const [user, setUser] = useState(user)
+
+  useEffect( () => {
+    axios.get(`/api/classrooms/${classroomId}/enrollments/51`)
+      .then( res => setEnrollment(res.data) )
+      .catch( err => console.log(err) )
+  }, [])
 
   return(
     <>
-      <h3>000</h3>
+      <h1>{enrollment.total_points}</h1>
+       {/* { enrollments.length > 0 ? 
+//         <>
+//           { enrollments.map( e => 
+//             <>
+//               <p key={e.id}>{e.total_points}</p>
+//             </>  
+//           )}
+//         </>
+//       : <p>No enrollments connected</p>} */}
     </>
   )
 }
 
-const ConnectedEnrollment = (props) => (
-  <EnrollmentConsumer>
-    { value => (
-      <Enrollment {...props} {...value} />
-    )}
-  </EnrollmentConsumer>
-)
+// const ConnectedEnrollment = (props) => (
+//   <AuthConsumer>
+//     { value => (
+//       <Enrollment {...props} {...value} />
+//     )}
+//   </AuthConsumer>
+// )
+// const ConnectedEnrollment = (props) => (
+//   <AuthConsumer>
+//     { value => 
+//       <Enrollment {...props} {...value} />
+//     }
+//   </AuthConsumer>
+// )
 
-export default ConnectedEnrollment
+// export default ConnectedEnrollment;
+
+export default Enrollment
