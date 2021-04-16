21c2a101e89e86585d2de9c5c5f54892c1214f87
Sean Schmidt <schmidtsean048@gmail.com>
2021-04-16 15:02:52 -0600

    new comonents for dropdown

---
+++ client/src/components/classrooms/DropDownClassroom.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import { Card, Button, Icon } from "semantic-ui-react";
+import {ClassroomConsumer} from '../../providers/ClassroomProvider';
+import UpdateClassroom from './UpdateClassroom';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
+
+const DropDownClassroom = ({e}) => {
+  const [classroom, setClassroom] = useState([])
+
+useEffect( () => {
+  axios.get(`/api/classrooms/${e.classroom_id}`)
+    .then( res => setClassroom(res.data))
+    .catch( err => console.log(err))
+}, [])
+
+return(
+  
+    <Card key={classroom.id}>
+      <Card.Content>
+        <Card.Header>
+          {classroom.name}
+        </Card.Header>
+      </Card.Content>
+      <Card.Content>
+        <Button>
+          <Link to={{
+            pathname: '/Dashboard',
+            state: {
+              classroomId: classroom.id,
+              enrollmentId: e.id
+            }
+          }}>
+            Select
+          </Link>
+        </Button>
+        </Card.Content>
+        </Card>
+        
+  
+)
+}
+
+export default DropDownClassroom;
