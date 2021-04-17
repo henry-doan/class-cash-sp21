c15b0185b8a6072536111377e9b3dbfc7818e17a
Harry Dhillon <navbirsdhillon@gmail.com>
2021-04-17 17:33:35 -0600

    created points and rewards controller

--- app/controllers/api/points_controller.rb
+++ app/controllers/api/points_controller.rb
@@ -1,2 +1,43 @@
 class Api::PointsController < ApplicationController
+  before_action :set_enrollment
+  before_action :set_point, only: [ :create, :update, :destroy]
+  
+  def index
+    render json: @enrollment.points
+  end
+
+  def create
+    if @point.save
+      render json: @point
+    else
+      render json: { errors: @point.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @point.update(point_params)
+      render json: @point
+    else
+      render json: { errors: @point.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @point.destroy
+    render json: { message: 'Point Removed!' }
+  end
+
+  private
+
+    def set_point
+      @point = @enrollment.points.find(params[:id])
+    end
+
+    def point_params
+      params.require(:point).permit(:name, :desc, :value, :enrollment_id)
+    end
+
+    def set_enrollment
+      @enrollment = Enrollment.find(params[:enrollment_id])
+    end
 end
--- app/controllers/api/rewards_controller.rb
+++ app/controllers/api/rewards_controller.rb
@@ -1,2 +1,43 @@
 class Api::RewardsController < ApplicationController
+  before_action :set_enrollment
+  before_action :set_reward, only: [ :create, :update, :destroy]
+  
+  def index
+    render json: @enrollment.rewards
+  end
+
+  def create
+    if @reward.save
+      render json: @reward
+    else
+      render json: { errors: @reward.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @reward.update(reward_params)
+      render json: @reward
+    else
+      render json: { errors: @reward.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @reward.destroy
+    render json: { message: 'Reward Removed!' }
+  end
+
+  private
+
+    def set_reward
+      @point = @enrollment.rewards.find(params[:id])
+    end
+
+    def reward_params
+      params.require(:reward).permit(:name, :cost, :desc, :enrollment_id)
+    end
+
+    def set_enrollment
+      @enrollment = Enrollment.find(params[:enrollment_id])
+    end
 end
