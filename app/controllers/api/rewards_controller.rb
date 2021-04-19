4af434c5a8740677525b3486d87ad775724b7155
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-19 17:58:03 -0600

    tried to get classroomRewards working

--- app/controllers/api/rewards_controller.rb
+++ app/controllers/api/rewards_controller.rb
@@ -1,5 +1,6 @@
 class Api::RewardsController < ApplicationController
   before_action :set_enrollment
+  # before_action :set_enrollments
   before_action :set_reward, only: [ :create, :update, :destroy]
   
   def index
@@ -27,6 +28,29 @@ class Api::RewardsController < ApplicationController
     render json: { message: 'Reward Removed!' }
   end
 
+  def classroomRewards
+    @enrollments = Enrollment.where(['classroom_id = ?', params[:classroom_id]])
+    @classroomRewards = []
+    # @connection = ActiveRecord::Base.connection
+
+    # @classroomRewards = @connection.exec_query("
+    #   select r.name, r.cost, r.desc
+    #   from rewards as r
+    #   left join enrollments as e
+    #   on r.enrollment_id = e.id
+    #   where e.classroom_id = 1
+    # ")
+    # # @classroomRewards = Reward.left_outer_joins(:enrollment).where('classroom_id = ?', params[:classroom_id])
+    # # @classroomRewards = Reward.includes(:enrollment).where(enrollment: { classroom_id: params[:classroom_id] })
+    # # render json: @classroomRewards
+    # return @classroomRewards
+    @enrollments.each |i| do
+      @classroomRewards.append(i.rewards)
+    end
+
+    render json: @clasroomRewards
+  end
+
   private
 
     def set_reward
@@ -39,5 +63,9 @@ class Api::RewardsController < ApplicationController
 
     def set_enrollment
       @enrollment = Enrollment.find(params[:enrollment_id])
+    end
+
+    def set_enrollments
+      @enrollments = Enrollment.where(['classroom_id = ?', params[:classroom_id]])
     end
 end
