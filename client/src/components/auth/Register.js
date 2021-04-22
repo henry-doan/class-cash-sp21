f126f4efc71a1fa4ac7a21201c9eb62c8e6a76ba
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-22 15:41:55 -0600

    Register styling

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import { useState } from 'react';
 import { AuthConsumer } from "../../providers/AuthProvider";
-import { Button, Form, Segment, Header } from 'semantic-ui-react';
+import { Button, Form, Segment, Header, Divider, Grid, Container } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import Logo from '../images/Logo.png'
+import { SharedForm, LeftAlignDiv, MainLogo, GreenButton, GreyButton } from '../../styledComponents/SharedStyles'
+
 const Register = ({ handleRegister, history }) => {
   const [user, setUser] = useState({ name: '', email: '', password: '', passwordConfirmation: '', isAdmin: false }) 
   const handleSubmit = (e) => {
@@ -13,54 +16,72 @@ const Register = ({ handleRegister, history }) => {
      }
   }
   return (
-    <Segment basic>
-      <Header as='h1' textAlign='center'>Register</Header>
-      <Form onSubmit={handleSubmit}>
-      <Form.Input
-          label="Name"
-          required
-          autoFocus
-          name='name'
-          value={user.name}
-          placeholder='name'
-          onChange={(e, { value }) => setUser({ ...user, name: value })}
-        />
-        <Form.Input
-          label="Email"
-          required
-          autoFocus
-          name='email'
-          value={user.email}
-          placeholder='Email'
-          onChange={(e, { value }) => setUser({ ...user, email: value })}
-        />
-        <Form.Input
-          label="Password"
-          required
-          name='password'
-          value={user.password}
-          placeholder='Password'
-          type='password'
-          onChange={(e, { value }) => setUser({ ...user, password: value })}
-        />
-        <Form.Input
-          label="Password Confirmation"
-          required
-          name='passwordConfirmation'
-          value={user.passwordConfirmation}
-          placeholder='Password Confirmation'
-          type='password'
-          onChange={(e, { value }) => setUser({ ...user, passwordConfirmation: value })}
-        />
-        <Segment textAlign='center' basic>
-          <Button style={{color: '#ffffff', backgroundColor: '#1CB993' }} type='submit'>Register</Button>
-          <Button>
-            <Link to="/login" style={{color: 'black'}} >
-              Login to an existing account
-            </Link>
-          </Button>
-        </Segment>
-      </Form>
+    <Segment basic padded="very">
+      <Divider hidden />
+      <MainLogo src={Logo} centered/>
+      <Divider hidden />
+      <Divider hidden />
+      <Divider hidden />
+      <Container>
+        <Grid>
+          <Grid.Row centered>
+            <SharedForm onSubmit={handleSubmit}>
+            <LeftAlignDiv>
+                <label>Name</label>
+              </LeftAlignDiv>
+            <Form.Input
+                required
+                autoFocus
+                name='name'
+                value={user.name}
+                placeholder='Name'
+                onChange={(e, { value }) => setUser({ ...user, name: value })}
+              />
+              <LeftAlignDiv>
+                <label>Email</label>
+              </LeftAlignDiv>
+              <Form.Input
+                required
+                autoFocus
+                name='email'
+                value={user.email}
+                placeholder='Email'
+                onChange={(e, { value }) => setUser({ ...user, email: value })}
+              />
+              <LeftAlignDiv>
+                <label>Password</label>
+              </LeftAlignDiv>
+              <Form.Input
+                required
+                name='password'
+                value={user.password}
+                placeholder='Password'
+                type='password'
+                onChange={(e, { value }) => setUser({ ...user, password: value })}
+              />
+              <LeftAlignDiv>
+                <label>Password Confirmation</label>
+              </LeftAlignDiv>
+              <Form.Input
+                required
+                name='passwordConfirmation'
+                value={user.passwordConfirmation}
+                placeholder='Password Confirmation'
+                type='password'
+                onChange={(e, { value }) => setUser({ ...user, passwordConfirmation: value })}
+              />
+              <LeftAlignDiv basic>
+                <GreenButton type='submit'>Register</GreenButton>
+                <Link to="/login">
+                  <GreyButton>
+                      Login to an existing account
+                  </GreyButton>
+                </Link>
+              </LeftAlignDiv>
+            </SharedForm>
+          </Grid.Row>
+        </Grid>
+      </Container>
     </Segment>
   )
 }
