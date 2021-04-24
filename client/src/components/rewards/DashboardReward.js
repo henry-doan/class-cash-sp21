eec7926b81d8f5a60b028fac08a2945380bd603d
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-04-24 09:57:12 -0600

    not enough points

--- client/src/components/rewards/DashboardReward.js
+++ client/src/components/rewards/DashboardReward.js
@@ -1,47 +1,22 @@
 import { Card, Grid, Button } from "semantic-ui-react";
-
 import { RewardConsumer } from '../../providers/RewardProvider';
-import { AuthConsumer } from '../../providers/AuthProvider';
 import { useState } from 'react';
 import UpdateReward from './UpdateReward';
 
 const Reward = ({r, enrollmentId, updateReward, user}) => {
   const [reward, setReward] = useState({name: r.name, cost: r.cost, desc: r.desc, redeemed: true, enrollment_id: r.enrollment_id})
   const [editing, setEditing] = useState(false)
-  const isAdmin = user.isAdmin
 
   const handleRedeem = (e) => {
     e.preventDefault()
     updateReward(r.id, enrollmentId, reward)
     window.location.reload()
   }
+
   return(
     <>
-{   !r.redeemed? 
-  <> { editing? 
-      <UpdateReward {...r} setEditing={setEditing}/>
-      :
-    <>{ isAdmin? 
-      <Grid.Row>
-        <Grid.Column>
-          <Card>
-            <Card.Content>
-              <Card.Header>
-                {r.name}
-              </Card.Header>
-              <Card.Description>
-                {r.desc}
-              </Card.Description>
-              <br/>
-              <Card.Meta>
-                {r.cost}
-              </Card.Meta>
-            </Card.Content>
-           
-          </Card>
-        </Grid.Column>
-      </Grid.Row>
-      : 
+{   !r.redeemed ? 
+  <>
       <Grid.Row>
         <Grid.Column>
           <Card>
@@ -63,28 +38,20 @@ const Reward = ({r, enrollmentId, updateReward, user}) => {
           </Card>
         </Grid.Column>
       </Grid.Row>
-}</>
-}</>: ""
-}</>
-)}
+  </> 
+  : "" }
+  </>
+  )
+}
 
 const ConnectedReward = (props) => (
-  <AuthConsumer>
+  <RewardConsumer>
     {
       value => (
         <Reward {...props} {...value} />
       )
     }
-  </AuthConsumer>
+  </RewardConsumer>
 )
 
-const AuthConnectedReward = (props) => (
-  <AuthConsumer>
-    {
-      value => (
-        <ConnectedReward {...props} {...value} />
-      )
-    }
-  </AuthConsumer>
-)
-export default AuthConnectedReward;
+export default ConnectedReward;
