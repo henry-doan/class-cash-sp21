4e4d7020030baafe6f1c05beecb1f34db52ac2ac
Sean Schmidt <schmidtsean048@gmail.com>
2021-04-28 19:18:42 -0600

    sticky footer fixed

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -2,16 +2,17 @@ import { Link } from "react-router-dom";
 import { Container, Divider, Icon } from "semantic-ui-react";
 import {Foot} from '../../styledComponents/SharedStyles';
 const Footer = () => (
-      // <Foot>
-      <Container textAlign='center' className='footer'>
       
+      <Container textAlign='center' className='footer'>
+      <Foot>
       <Divider />
       <Link to='/About'>About Us</Link>
       <Container class="copyright">Copyright©2021. All rights reserved.</Container>
       
       
       <Divider hidden />
+      </Foot>
       </Container>
-      // </Foot>
+      
 )
 export default Footer;
